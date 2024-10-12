659483eace90fbd2c166bf0a247d0254764a56d3
Anna Kozachenko <annakozacenko@yandex.ru>
2024-10-12 21:42:52 +0300

    на проверку

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const cardsList = document.querySelector('.places__list');
+const addCardButton = document.querySelector('.profile__add-button');
 // @todo: Функция создания карточки
-
+function addCard(name, link, deleteFunction) {
+    const card = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardTitle = card.querySelector('.card__title');
+    const cardImage = card.querySelector('.card__image');
+    const cardDeleteButton = card.querySelector('.card__delete-button');
+    cardTitle.textContent = name;
+    cardImage.src = link;
+    cardDeleteButton.addEventListener('click', deleteFunction);
+    return card;
+}
 // @todo: Функция удаления карточки
-
+function deleteCard(event) {
+    const cardToDelete = event.target.closest('.card');
+    cardToDelete.remove();
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach((card) => {
+    cardsList.append(addCard(card.name, card.link, deleteCard));
+});
