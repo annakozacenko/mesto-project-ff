703e12db5a47154e57eee59e57117e9152f7ab37
Anna Kozachenko <annakozacenko@yandex.ru>
2024-10-20 18:52:29 +0300

    господи, настройка готова

--- webpack.config.js
+++ webpack.config.js
@@ -41,9 +41,13 @@ module.exports = {
     // при обработке этих файлов нужно использовать
     // MiniCssExtractPlugin.loader и css-loader
     use: [MiniCssExtractPlugin.loader, {
-      loader: 'css-loader'
+      loader: 'css-loader',
+          // добавьте объект options
+    options: { importLoaders: 1 }
     }]
-  }
+  },
+      // Добавьте postcss-loader
+      'postcss-loader'
       ]
   },
   plugins: [
